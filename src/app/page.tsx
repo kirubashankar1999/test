bd89ac5a6b43d37db95eb8e529ca8780f6a9dbce
kirubashankar1999 <82397157+kirubashankar1999@users.noreply.github.com>
2023-07-04 20:54:02 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,278 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="relative bg-white w-full h-[4974px] overflow-hidden text-left text-xl text-black font-inter">
+      <div className="absolute top-[2526px] left-[0px] bg-gray w-[1440px] h-[972px]" />
+      <div className="absolute top-[1116px] left-[0px] bg-navajowhite w-[1466px] h-[651px]" />
+      <div className="absolute top-[1333px] left-[220px] rounded-[50%] bg-navajowhite w-[297px] h-[299px]" />
+      <Image
+        className="absolute top-[1108px] left-[29px] w-[659px] h-[581.41px] overflow-hidden"
+        alt=""
+        width={659}
+        height={581.41}
+        src="/objects.svg"
+      />
+      <div className="absolute top-[201px] left-[calc(50%_-_226px)] text-21xl font-medium text-center">
+        Welcome to Lazt Wordz
+      </div>
+      <div className="absolute top-[1321px] left-[760px] leading-[174.02%] inline-block w-[511px]">
+        ... of eternal connections and leave a heartfelt message for your loved
+        ones to cherish forever. Last Words is here to provide you with a unique
+        opportunity to express your deepest emotions and thoughts, even beyond
+        the boundaries of life itself. Our simple and intuitive platform allows
+        you to create a lasting testament, ensuring your voice resonates with
+        those you hold dear
+      </div>
+      <div className="absolute top-[267px] left-[calc(50%_-_448px)] text-[72px] leading-[125%] font-extrabold font-nunito text-royalblue text-center inline-block w-[896px]">
+        Leaving a Lasting Legacy
+      </div>
+      <div
+        id="howItWork"
+        className="absolute top-[1258px] left-[calc(50%_+_40px)] text-21xl leading-[125%] font-extrabold font-nunito inline-block w-[600px]"
+      >
+        Unlock the power...
+      </div>
+      <Image
+        className="absolute top-[524px] left-[calc(50%_-_278px)] w-[555px] h-[426px] overflow-hidden"
+        alt=""
+        src="/frame.svg"
+        width={555}
+        height={426}
+      />
+      <div className="absolute top-[392px] left-[calc(50%_-_148px)] w-[314px] h-[58px] text-center">
+        <div className="absolute top-[0px] left-[calc(50%_-_157px)] flex flex-row items-center justify-start gap-[12px]">
+          {/* <Link href="#howItWork"> */}
+          <Image
+            src={"/scrollDown.gif"}
+            alt="scroll down "
+            width={58}
+            height={58}
+          />
+          {/* </Link> */}
+          <div className="relative leading-[174.02%] inline-block w-[244px] shrink-0">
+            Scroll down to know how
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[946px] left-[1095px] rounded-[50%] [background:linear-gradient(180deg,_#fce5e8,_#ff5972)] w-[125px] h-[125px]" />
+      <div className="absolute top-[304px] left-[65px] rounded-[50%] [background:linear-gradient(180deg,_#fce5e8,_#ff5972)] w-[106px] h-[106px]" />
+      <div className="absolute top-[630.3px] left-[1127px] rounded-[50%] [background:linear-gradient(180deg,_#9da8ea,_#eff1fa)] w-[34px] h-[34px] [transform:_rotate(111.2deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[853px] left-[-12px] rounded-[50%] [background:linear-gradient(180deg,_#9da8ea,_#fefefe)] w-[159px] h-[159px]" />
+      <div className="absolute top-[1840px] left-[calc(50%_-_300px)] text-21xl leading-[125%] font-extrabold font-nunito text-center inline-block w-[600px]">
+        Why Choose Lazt Wordz?
+      </div>
+      <div className="absolute top-[2593px] left-[calc(50%_-_300px)] text-21xl leading-[125%] font-extrabold font-nunito text-center inline-block w-[600px]">
+        Our Pricings
+      </div>
+      <div className="absolute top-[4367px] left-[167px] w-[600px] h-[468px]">
+        <div className="absolute top-[68px] left-[3px] leading-[174.02%] inline-block w-[537px]">
+          <p className="m-0">
+            We understand that embarking on this journey of leaving your last
+            words can be emotionally overwhelming. Our dedicated support team is
+            here to assist you every step of the way. If you have any questions,
+            concerns, or need guidance, please don&apos;t hesitate to reach out
+            to us.
+          </p>
+        </div>
+        <div className="absolute top-[398px] left-[3px] leading-[174.02%] inline-block w-[537px] text-mediumslateblue">
+          <p className="m-0">
+            <b>
+              <span className="text-black">{`Email: `}</span>
+              <span className="[text-decoration:underline] cursor-pointer">
+                info@lastwordz.in
+              </span>
+            </b>
+          </p>
+          <p className="m-0 text-black">
+            <b>
+              <span>Phone</span>
+            </b>
+            <span>
+              <span className="font-inter">{`: `}</span>
+            </span>
+            <b className="[text-decoration:underline] font-inter text-mediumslateblue cursor-pointer">
+              +91 98765 43210
+            </b>
+          </p>
+        </div>
+        <div className="absolute top-[0px] left-[calc(50%_-_300px)] text-21xl leading-[125%] font-extrabold font-nunito inline-block w-[600px]">
+          Contact Us
+        </div>
+      </div>
+      <div className="absolute top-[1963px] left-[171px] rounded-2xl bg-lavender w-[521px] h-[198px]" />
+      <div className="absolute top-[2187px] left-[171px] rounded-2xl bg-lemonchiffon w-[521px] h-[198px]" />
+      <div className="absolute top-[1963px] left-[720px] rounded-2xl bg-mistyrose-200 w-[521px] h-[198px]" />
+      <div className="absolute top-[2187px] left-[720px] rounded-2xl bg-lightcyan w-[521px] h-[198px]" />
+      <div className="absolute top-[1994px] left-[calc(50%_-_339px)] leading-[125%] font-extrabold font-nunito inline-block w-[278px]">
+        A Timeless Gift of Love
+      </div>
+      <div className="absolute top-[2227px] left-[calc(50%_-_341px)] leading-[125%] font-extrabold font-nunito inline-block w-[278px]">
+        Keep Your Voice Alive
+      </div>
+      <div className="absolute top-[1994px] left-[calc(50%_+_210px)] leading-[125%] font-extrabold font-nunito inline-block w-[278px]">{`Easy & Intuitive`}</div>
+      <div className="absolute top-[2218px] left-[calc(50%_+_210px)] leading-[125%] font-extrabold font-nunito inline-block w-[278px]">{`Safe & Secure`}</div>
+      <div className="absolute top-[2030px] left-[381px] text-sm leading-[174.02%] inline-block w-[278px]">
+        Leave a heartfelt gift for loved ones. Express love, share wisdom, and
+        preserve cherished memories. Last Words keeps your essence alive.
+      </div>
+      <div className="absolute top-[2263px] left-[379px] text-sm leading-[174.02%] inline-block w-[291px]">
+        Record personalised messages with advanced technology. Loved ones can
+        hear and see you, feeling close and comforted.
+      </div>
+      <div className="absolute top-[2030px] left-[930px] text-sm leading-[174.02%] inline-block w-[278px]">
+        <p className="m-0">
+          Our user-friendly platform makes sharing last words simple. No
+          technical expertise needed. Sign up and leave your legacy
+          effortlessly.
+        </p>
+      </div>
+      <div className="absolute top-[2254px] left-[930px] text-sm leading-[174.02%] inline-block w-[278px]">
+        Your messages are private and secure. We prioritize your trust with
+        cutting-edge encryption.
+      </div>
+      <Image
+        className="absolute top-[2000px] left-[209px] w-32 h-[124px]"
+        alt=""
+        src="/group-1.svg"
+        width={32}
+        height={124}
+      />
+      <Image
+        className="absolute top-[2218px] left-[214px] w-[114px] h-[127px]"
+        alt=""
+        src="/group-3.svg"
+        width={114}
+        height={127}
+      />
+      <Image
+        className="absolute top-[2000px] left-[772px] w-[114px] h-[121px]"
+        alt=""
+        width={114}
+        height={121}
+        src="/group-2.svg"
+      />
+      <Image
+        className="absolute top-[2219px] left-[764px] w-[113px] h-[137px]"
+        alt=""
+        width={113}
+        height={137}
+        src="/group-4.svg"
+      />
+      <Image
+        className="absolute top-[2013px] left-[781px] w-24 h-24 object-cover"
+        alt=""
+        width={24}
+        src="/easytouse-1@2x.png"
+        height={24}
+      />
+      <Image
+        className="absolute top-[2251px] left-[247px] w-[66px] h-[66px] object-cover"
+        alt=""
+        src="/voicesearch-1@2x.png"
+        width={66}
+        height={66}
+      />
+      <Image
+        className="absolute top-[2227px] left-[772px] w-[97px] h-[97px] object-cover"
+        alt=""
+        src="/cybersecurity-1@2x.png"
+        width={97}
+        height={97}
+      />
+      <div className="absolute top-[283px] left-[1271px] rounded-[50%] [background:linear-gradient(212.77deg,_#fdf6f7,_#ff5972)] w-[219px] h-[219px]" />
+      <Image
+        className="absolute top-[4392px] left-[807px] w-[500px] h-[333px] overflow-hidden"
+        alt=""
+        src="/contact-uscuate-1.svg"
+        width={500}
+        height={333}
+      />
+      <Image
+        className="absolute top-[4638px] cursor-pointer left-[164px] w-[274px] h-[95px] object-cover"
+        alt=""
+        src="/whatsappbutton-1@2x.png"
+        width={274}
+        height={95}
+      />
+      <div className="absolute bottom-[-4803px] left-[7503px] bg-royalblue w-[1451px] h-[193px] overflow-hidden text-sm text-white">
+        <div className="absolute top-[92.63px] left-[736.63px] w-[15.02px] h-[15.02px] overflow-hidden" />
+        <div className="absolute top-[35px] left-[calc(50%_-_309.15px)] w-[639px] h-[17px]">
+          <div className="absolute top-[0px] left-[calc(50%_-_319.5px)] flex flex-row items-start justify-start gap-[64px]">
+            <div className="relative">Disclaimer</div>
+            <div className="relative">Privacy Policy</div>
+            <div className="relative">{`Terms & Conditions`}</div>
+            <div className="relative">Contact</div>
+            <div className="relative">About</div>
+          </div>
+        </div>
+        <Image
+          className="absolute top-[81px] left-[655px] w-[142px] h-6"
+          alt=""
+          src="/group-2007.svg"
+          width={655}
+          height={6}
+        />
+        <div className="absolute top-[127.5px] left-[calc(50%_-_190px)] box-border w-[381px] h-px border-t-[1px] border-solid border-white" />
+        <div className="absolute top-[148px] left-[calc(50%_-_86.5px)] text-[12.67px] font-medium text-center">
+          ®Copyright 2023 LaztWordz
+        </div>
+      </div>
+      <div className="absolute top-[3634px] left-[221px] bg-gainsboro w-[959px] h-[597px]" />
+      <div className="absolute top-[3914px] left-[calc(50%_-_367px)] text-21xl font-extrabold font-nunito">{`FAQ’s & How it works (Video Tutorial)`}</div>
+      <Image
+        className="absolute top-[2700px] left-[172px] w-[1096px] h-[687px] object-cover"
+        alt=""
+        width={1096}
+        height={687}
+        src="/hero-pricing-1-1@2x.png"
+      />
+      <div className="absolute top-[0px] left-[0px] w-[1440px] h-28 text-center text-lg">
+        <div className="absolute top-[0px] left-[0px] bg-mistyrose-100 w-[1440px] h-28" />
+        <div className="absolute top-[37px] left-[837px] flex flex-row items-center justify-start gap-[24px]">
+          <div className="relative cursor-pointer font-medium flex items-center justify-center w-[77px] h-[38px] shrink-0">
+            Why Us
+          </div>
+          <div className="relative cursor-pointer font-medium flex items-center justify-center w-[113px] h-[38px] shrink-0">
+            Our Pricing
+          </div>
+          <div className="relative cursor-pointer font-medium flex items-center justify-center w-[109px] shrink-0">
+            Contact Us
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[32px] left-[1206px] w-[175px] h-12 text-center text-[16px] text-white">
+        <div className="absolute top-[0px] left-[0px] rounded-[62px] bg-royalblue w-[175px] h-12" />
+        <div className="absolute top-[14px] left-[27px] cursor-pointer font-medium">
+          Sign up for Free
+        </div>
+      </div>
+      <Image
+        className="absolute top-[24px] left-[65px] "
+        alt=""
+        src="/logo.svg"
+        width={200}
+        height={120}
+      />
+      {/* <Image
+        className="absolute h-[0.38%] w-[4.87%] top-[0.6%] right-[85.68%] bottom-[99.02%] left-[9.44%] max-w-full overflow-hidden max-h-full"
+        alt=""
+        src="/group-25.svg"
+        fill
+      />
+      <Image
+        className="absolute h-[0.36%] w-[6.88%] top-[1.05%] right-[83.68%] bottom-[98.59%] left-[9.44%] max-w-full overflow-hidden max-h-full"
+        alt=""
+        src="/group-26.svg"
+        fill
+      />
+      <Image
+        className="absolute h-[0.17%] w-[5.57%] top-[1.53%] right-[84.99%] bottom-[98.3%] left-[9.44%] max-w-full overflow-hidden max-h-full"
+        alt=""
+        src="/group.svg"
+        fill
+      /> */}
+    </div>
+  );
+}
